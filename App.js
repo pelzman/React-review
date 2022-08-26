8dead1141dd9a36a354b1844782c8c9821c7b203
pelzman <107943338+pelzman@users.noreply.github.com>
2022-08-26 23:16:42 +0530

    React-review

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Review from './Reviews';
+
+import './App.css';
+
+function App() {  
+
+  return (
+    <main>
+      <section className='container'>
+        <div className="title">
+          <h2>Our Reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <Review/>
+
+      </section>
+    </main> 
+    
+  
+  );
+}
+
+export default App;
